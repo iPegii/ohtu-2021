b819627c1a885815486aa7aa58ceaea1fb199797
iPegii <iPegii@users.noreply.github.com>
2021-04-26 20:51:31 +0300

    osa 8

--- viikko3/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/WebLogin/src/main/java/ohtu/Tester.java
@@ -4,11 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.Random;
 
 public class Tester {
 
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
+/*
+    // epäonnistunut kirjautuminen: oikea käyttäjätunnus, väärä salasana
 
         driver.get("http://localhost:4567");
         
@@ -31,6 +34,66 @@ public class Tester {
         sleep(3);
         
         driver.quit();
+*/
+/*
+    // uuden käyttäjätunnuksen luominen
+
+        driver.get("http://localhost:4567");
+        
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("login"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+        
+        driver.quit();
+
+*/
+// uuden käyttäjätunnuksen luomisen jälkeen tapahtuva ulkoskirjautuminen sovelluksesta
+        driver.get("http://localhost:4567");
+        
+        sleep(2);
+        
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("arto"+r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("uusisalasana");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("uusisalasana");
+        element = driver.findElement(By.name("signup"));
+        
+        sleep(2);
+        element.submit();
+
+        sleep(3);
+
+        element = driver.findElement(By.name("continueMainpage"));
+        element.click();
+        sleep(2);
+        
+        element = driver.findElement(By.name("logout"));
+        element.click();
+        sleep(2);
+        
+        driver.quit();
+        
     }
     
     private static void sleep(int n){
